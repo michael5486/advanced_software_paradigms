95b1d1074d326d59d13227a42338138f510f7ab7
EC2 Default User <ec2-user@ip-172-31-33-181.ec2.internal>
2018-02-12 18:19:58 +0000

    finished Scala

--- Scala/matrix.scala
+++ Scala/matrix.scala
@@ -1,30 +1,77 @@
 object matrix{
+  
   def main(args: Array[String]) {
-    println("testing")
-
-      
+    //creates random 5x5 matrix of ints
+    val matrix = createMatrix()
+    //prints matrix
+    printMatrix(matrix)
+    //transposes matrix
+    val transposed = transposeMatrix(matrix)
+    //prints transposed matrix
+    printMatrix(transposed)
+    //converts matrix to a list
+    val list = convertMatrix(transposed)
+    //prints list
+    print("\n" + list.toString)
+    //sorts list
+    val sorted = list.sorted
+    //prints sorted list
+    print("\n" + sorted.toString)
+    //prints sorted list in english words
+    printWords(sorted)
   }
   
-  def createMatrix():Array = {
-    val matrix = Array.ofDim[Int](5,5)
+  def createMatrix(): Array[Array[Int]] = {
+    var matrix = Array.ofDim[Int](5,5)
     var i = 0
     var j = 0
     var r = scala.util.Random
-    for (i <-0 to 5){
-        for (j <-0 to 5){
-            matrix(i)(j) = r.nextInt(9) + 1
+    for (i <-0 to 4){
+        for (j <-0 to 4){
+            matrix(i)(j) = r.nextInt(8) + 1
         }
     }
     return matrix
   }
   
-  def printMatrix(matrix: Array): Unit =  {
-    //   val i, j = 0
-    //   for (i<=5){
-    //       for (j<-5){
-    //           print
-    //       }
-    //   }
-    println(matrix)
+  def transposeMatrix(matrix: Array[Array[Int]]): Array[Array[Int]] = {
+    var transposed = Array.ofDim[Int](5,5)
+    var i = 0
+    var j = 0
+    for (i <-0 to 4){
+        for (j <-0 to 4){
+            transposed(i)(j) = matrix(j)(i)
+        }
+    }
+    return transposed
+  }
+  
+  def convertMatrix(matrix: Array[Array[Int]]): List[Int] = {
+    var list: List[Int] = List()
+    for (i <- 4 to 0 by -1){
+      list = list.:::(matrix(i).toList)
+    }
+    return list
+  }
+  
+  def printMatrix(matrix: Array[Array[Int]]): Unit =  {
+    var i = 0
+    var j = 0
+    for (i <-0 to 4){
+      print("\n[")
+        for (j <-0 to 4){
+          print(" ")
+          print(matrix(i)(j))
+        }
+      print(" ]")
+    }
+    print("\n")
+  }
+  
+  def printWords(list: List[Int]): Unit = {
+    val strings = List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    for (i<-0 to 24) {
+      println(strings(list(i) - 1))
+    }
   }
 }
